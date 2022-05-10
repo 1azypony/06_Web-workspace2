bb71a8706978a231325379e7f71042a2b54a489e
1azypony <105184663+1azypony@users.noreply.github.com>
2022-05-10 18:12:58 +0900

    Add files via upload

--- src/com/kh/notice/model/service/NoticeService.java
+++ src/com/kh/notice/model/service/NoticeService.java
@@ -2,7 +2,9 @@ package com.kh.notice.model.service;
 
 // JDBCTemplate 클래스의 모든 메소드를 가져다 쓰겠다.
 import static com.kh.common.JDBCTemplate.close;
+import static com.kh.common.JDBCTemplate.commit;
 import static com.kh.common.JDBCTemplate.getConnection;
+import static com.kh.common.JDBCTemplate.rollback;
 
 import java.sql.Connection;
 import java.util.ArrayList;
@@ -31,4 +33,107 @@ public class NoticeService {
 		// 5) 결과 반환
 		 return list;
 	}
+	// ----------------------------------------------------------------
+		// 공지사항 작성 서비스
+		// controller단에서 보내준 Notice n을 매개변수로 데려옴
+		// 그런데 결과가 처리된 행의 갯수로 담겨야하니까 int! 
+		public int insertNotice(Notice n) {
+			
+			// 1) Connection 객체 생성
+			Connection conn = getConnection();
+			
+			// 2)DAO로 conn과 전달값 넘기기
+			int result = new NoticeDao().insertNotice(conn, n);
+			// dao로 보낸 결과는 처리된 행의 갯수로 돌아오기 때문에 int result로 받기!!
+			
+			// 3) 결과에 따라 commit, rollback처리
+			if(result > 0) {
+				commit(conn);
+			}
+			else {
+				rollback(conn);
+			}
+			
+			// 4) Connection 객체 반납
+			close(conn);
+			
+			// 5) 결과 반환
+			return result;	
+		}
+		
+	// -------------------------------------------------------------
+	// 공지사항 조회수 증가용 서비스
+	public int increaseCount(int noticeNo) {
+			
+		// 1) Connection 객체 생성
+		Connection conn = getConnection();
+			
+		// 2) Dao로 보내 결과값 받기
+		int result = new NoticeDao().increaseCount(conn, noticeNo);
+			
+		// commit, rollback
+			if(result > 0) {
+			 commit(conn);
+		 }
+			else {
+			rollback(conn);
+			}
+			 
+			// Connection 객체 반납
+			close(conn);
+			 
+			// 결과 리턴 
+			return result;
+		}
+	// ----------------------------------------------------------------
+	// 공지사항 상세보기용 서비스
+	public Notice selectNotice(int NoticeNo) {
+			
+		Connection conn = getConnection();
+			
+		Notice n = new NoticeDao().selectNotice(conn, NoticeNo);
+			
+		close(conn);
+			
+		return n;
+	}
+	// ----------------------------------------------------------------
+	// 공지사항 수정하기용 서비스
+	public int updateNotice(Notice n) {
+		
+		Connection conn = getConnection();
+		
+		int result = new NoticeDao().updateNotice(conn, n);
+		
+		if(result > 0) {
+			commit(conn);
+		}
+		else {
+			rollback(conn);
+		}
+		
+		close(conn);
+		
+		return result;
+	}
+	// ---------------------------------------------------------------
+	// 공지사항 삭제용 서비스
+	public int deleteNotice(int noticeNo) {
+		
+		Connection conn = getConnection();
+		
+		int result = new NoticeDao().deleteNotice(conn, noticeNo);
+		
+		if(result > 0) {
+			commit(conn);
+		}
+		else {
+			rollback(conn);
+		}
+		
+		close(conn);
+		
+		return result;
+	}
 }
+
